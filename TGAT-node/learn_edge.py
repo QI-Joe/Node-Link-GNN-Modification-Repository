index overflow bug finished
update both embedding and neighbor finder

TGAT-node/learn_edge.py
@@ -317,7 +317,7 @@ for sp in range(VIEW):
                 tgan.eval()
                 break
             
-            tgan.ngh_finder = full_test_ngh_finder
+            tgan.test_emb_update(full_test_ngh_finder, t1_test.node_pos, t1_test.edge_pos)
             test_label_l = (t1_label_src, t1_laebl_dst)
             test_src, test_dst = eval_one_epoch(num_cls, tgan, test_rand_sampler, test_src_l, test_dst_l, test_ts_l, test_label_l, num_neighbors=NUM_NEIGHBORS)
 
@@ -335,6 +335,8 @@ for sp in range(VIEW):
             test_src["train_acc"], test_dst["train_acc"] = np.mean(trian_acc_src), np.mean(train_acc_dst)
             test_src["val_acc"], test_dst["val_acc"] = val_src["accuracy"], val_dst["accuracy"]
             score_recoder.extend([test_src, test_dst])
+
+            tgan.train_val_emb_restore()
 
 
 

TGAT-node/module.py
@@ -6,6 +6,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch import Tensor
+from graph import NeighborFinder
 
 class MergeLayer(torch.nn.Module):
     def __init__(self, dim1, dim2, dim3, dim4):
@@ -454,11 +455,33 @@ class TGAN(torch.nn.Module):
         # self.affinity_score = MergeLayer(self.feat_dim, self.feat_dim, self.feat_dim, 1) #torch.nn.Bilinear(self.feat_dim, self.feat_dim, 1, bias=True)
     
     def temproal_update(self, ngh_finder, n_feat, e_feat):
-        self.ngh_finder = ngh_finder
+        self.ngh_finder: NeighborFinder = ngh_finder
         self.n_feat_th = torch.nn.Parameter(torch.from_numpy(n_feat.astype(np.float32)))
         self.e_feat_th = torch.nn.Parameter(torch.from_numpy(e_feat.astype(np.float32)))
         self.edge_raw_embed = torch.nn.Embedding.from_pretrained(self.e_feat_th, padding_idx=0, freeze=True)
         self.node_raw_embed = torch.nn.Embedding.from_pretrained(self.n_feat_th, padding_idx=0, freeze=True)
+
+    def train_val_backup(self):
+        self.n_feat_th_backup = self.n_feat_th
+        self.e_feat_th_backup = self.e_feat_th
+        self.edge_raw_embed_backup = self.edge_raw_embed
+        self.node_raw_embed_backup = self.node_raw_embed
+
+    def train_val_emb_restore(self):
+        self.n_feat_th = self.n_feat_th_backup
+        self.e_feat_th = self.e_feat_th_backup
+        self.edge_raw_embed = self.edge_raw_embed_backup
+        self.node_raw_embed = self.node_raw_embed_backup
+
+    def test_emb_update(self, ngh_finder, n_feat, e_feat):
+        self.ngh_finder: NeighborFinder = ngh_finder
+        self.train_val_backup()
+        device = self.n_feat_th.device
+        # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.n_feat_th = torch.nn.Parameter(torch.from_numpy(n_feat.astype(np.float32)).to(device))
+        self.e_feat_th = torch.nn.Parameter(torch.from_numpy(e_feat.astype(np.float32)).to(device))
+        self.edge_raw_embed = torch.nn.Embedding.from_pretrained(self.e_feat_th, padding_idx=0, freeze=True).to(device)
+        self.node_raw_embed = torch.nn.Embedding.from_pretrained(self.n_feat_th, padding_idx=0, freeze=True).to(device)
 
     def forward(self, src_idx_l, target_idx_l, cut_time_l, num_neighbors=20):
         